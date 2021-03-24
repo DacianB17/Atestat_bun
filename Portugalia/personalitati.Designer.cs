0a11682cb257a482e908d56bdc1d21c6e54191c2
Dacian Beldean <dacian_beldean@yahoo.com>
2021-03-24 17:02:07 +0200

    persoanlitati-modificari

--- Portugalia/personalitati.Designer.cs
+++ Portugalia/personalitati.Designer.cs
@@ -28,47 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.blisabona = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(personalitati));
             this.rtbpersonalitati = new System.Windows.Forms.RichTextBox();
+            this.imagpersonalitati = new System.Windows.Forms.PictureBox();
+            this.bvasco = new System.Windows.Forms.Label();
+            this.bpedro = new System.Windows.Forms.Label();
+            this.bmaria = new System.Windows.Forms.Label();
+            this.bcristiano = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagpersonalitati)).BeginInit();
             this.SuspendLayout();
-            // 
-            // blisabona
-            // 
-            this.blisabona.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.blisabona.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.blisabona.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.blisabona.Location = new System.Drawing.Point(3, 2);
-            this.blisabona.Name = "blisabona";
-            this.blisabona.Size = new System.Drawing.Size(105, 60);
-            this.blisabona.TabIndex = 2;
-            this.blisabona.Text = "Vasco da Gama";
-            this.blisabona.UseVisualStyleBackColor = false;
-            this.blisabona.Click += new System.EventHandler(this.blisabona_Click);
             // 
             // rtbpersonalitati
             // 
-            this.rtbpersonalitati.Location = new System.Drawing.Point(132, 12);
+            this.rtbpersonalitati.Location = new System.Drawing.Point(106, 73);
             this.rtbpersonalitati.Name = "rtbpersonalitati";
-            this.rtbpersonalitati.Size = new System.Drawing.Size(486, 412);
+            this.rtbpersonalitati.ReadOnly = true;
+            this.rtbpersonalitati.Size = new System.Drawing.Size(470, 563);
             this.rtbpersonalitati.TabIndex = 7;
             this.rtbpersonalitati.Text = "";
+            this.rtbpersonalitati.Visible = false;
+            // 
+            // imagpersonalitati
+            // 
+            this.imagpersonalitati.Location = new System.Drawing.Point(595, 73);
+            this.imagpersonalitati.Name = "imagpersonalitati";
+            this.imagpersonalitati.Size = new System.Drawing.Size(572, 563);
+            this.imagpersonalitati.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagpersonalitati.TabIndex = 8;
+            this.imagpersonalitati.TabStop = false;
+            this.imagpersonalitati.Visible = false;
+            // 
+            // bvasco
+            // 
+            this.bvasco.AutoSize = true;
+            this.bvasco.Location = new System.Drawing.Point(309, 19);
+            this.bvasco.Name = "bvasco";
+            this.bvasco.Size = new System.Drawing.Size(109, 17);
+            this.bvasco.TabIndex = 9;
+            this.bvasco.Text = "Vasco da Gama";
+            this.bvasco.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bvasco.Click += new System.EventHandler(this.bvasco_Click);
+            // 
+            // bpedro
+            // 
+            this.bpedro.AutoSize = true;
+            this.bpedro.Location = new System.Drawing.Point(434, 19);
+            this.bpedro.Name = "bpedro";
+            this.bpedro.Size = new System.Drawing.Size(142, 17);
+            this.bpedro.TabIndex = 10;
+            this.bpedro.Text = "Pedro Alvares Cabral";
+            this.bpedro.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bpedro.Click += new System.EventHandler(this.bpedro_Click);
+            // 
+            // bmaria
+            // 
+            this.bmaria.AutoSize = true;
+            this.bmaria.Location = new System.Drawing.Point(592, 19);
+            this.bmaria.Name = "bmaria";
+            this.bmaria.Size = new System.Drawing.Size(50, 17);
+            this.bmaria.TabIndex = 11;
+            this.bmaria.Text = "Maria I";
+            this.bmaria.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bmaria.Click += new System.EventHandler(this.bmaria_Click);
+            // 
+            // bcristiano
+            // 
+            this.bcristiano.AutoSize = true;
+            this.bcristiano.Location = new System.Drawing.Point(658, 19);
+            this.bcristiano.Name = "bcristiano";
+            this.bcristiano.Size = new System.Drawing.Size(120, 17);
+            this.bcristiano.TabIndex = 12;
+            this.bcristiano.Text = "Cristiano Ronaldo";
+            this.bcristiano.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bcristiano.Click += new System.EventHandler(this.bcristiano_Click);
             // 
             // personalitati
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1211, 674);
+            this.Controls.Add(this.bcristiano);
+            this.Controls.Add(this.bmaria);
+            this.Controls.Add(this.bpedro);
+            this.Controls.Add(this.bvasco);
+            this.Controls.Add(this.imagpersonalitati);
             this.Controls.Add(this.rtbpersonalitati);
-            this.Controls.Add(this.blisabona);
             this.Name = "personalitati";
-            this.Text = "personalitati";
+            this.Text = "Personalitati";
+            ((System.ComponentModel.ISupportInitialize)(this.imagpersonalitati)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button blisabona;
         private System.Windows.Forms.RichTextBox rtbpersonalitati;
+        private System.Windows.Forms.PictureBox imagpersonalitati;
+        private System.Windows.Forms.Label bvasco;
+        private System.Windows.Forms.Label bpedro;
+        private System.Windows.Forms.Label bmaria;
+        private System.Windows.Forms.Label bcristiano;
     }
 }
--- Portugalia/personalitati.cs
+++ Portugalia/personalitati.cs
@@ -21,5 +21,41 @@ namespace Portugalia
         {
             rtbpersonalitati.LoadFile("vascodagama.rtf");
         }
+
+        private void bvasco_Click(object sender, EventArgs e)
+        {
+            rtbpersonalitati.Visible = true;
+            rtbpersonalitati.LoadFile("vascodagama.rtf");
+            imagpersonalitati.Visible = true;
+            Image image = Image.FromFile("vasco.jpg");
+            imagpersonalitati.Image = image;
+        }
+
+        private void bpedro_Click(object sender, EventArgs e)
+        {
+            rtbpersonalitati.Visible = true;
+            rtbpersonalitati.LoadFile("pedroalvarescabral.rtf");
+            imagpersonalitati.Visible = true;
+            Image image = Image.FromFile("pedro.jpg");
+            imagpersonalitati.Image = image;
+        }
+
+        private void bmaria_Click(object sender, EventArgs e)
+        {
+            rtbpersonalitati.Visible = true;
+            rtbpersonalitati.LoadFile("maria1.rtf");
+            imagpersonalitati.Visible = true;
+            Image image = Image.FromFile("maria.jpg");
+            imagpersonalitati.Image = image;
+        }
+
+        private void bcristiano_Click(object sender, EventArgs e)
+        {
+            rtbpersonalitati.Visible = true;
+            rtbpersonalitati.LoadFile("cristianoronaldo.rtf");
+            imagpersonalitati.Visible = true;
+            Image image = Image.FromFile("cristiano.jpg");
+            imagpersonalitati.Image = image;
+        }
     }
 }
